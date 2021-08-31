2df94209e81adb1be676172a39c0a0888376d49f
Oliver Heward <oliverhewardbie@gmail.com>
2021-08-31 23:32:22 +0100

    fixing filesystem issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pokemon-images`,
-        path: `${__dirname}/src/assets/pokemon/`,
-      },
-    },
+    `gatsby-source-filesystem`,
     {
       resolve: "gatsby-plugin-manifest",
       options: {
